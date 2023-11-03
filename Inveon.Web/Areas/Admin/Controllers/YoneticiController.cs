62731ea56673b903ad1e133c1472584d0b019f97
Fatih Hakan Mehmet CAYLIDEMIRCI <cyldmrcfhm@gmail.com>
2023-11-03 23:35:00 +0300

    add-microservice-signalr

---
+++ Inveon.Web/Areas/Admin/Controllers/YoneticiController.cs
@@ -0,0 +1,30 @@
+﻿using Inveon.Web.Hubs;
+using Inveon.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Inveon.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class YoneticiController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Git()
+        {
+            return View();
+        }
+        public IActionResult AdminLogout()
+        {
+            return SignOut("Cookies", "oidc");
+        }
+
+
+
+
+    }
+}
---
+++ Inveon.Web/Hubs/LiveChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Inveon.Web.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Inveon.Web.Hubs
+{
+    public class LiveChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            string timestamp = DateTime.Now.ToString("HH:mm:ss"); 
+            string messageWithTimestamp = $"{message}\n{timestamp.ToLower()}"; 
+            await Clients.All.SendAsync("ReceiveMessage", user, messageWithTimestamp);
+        }
+
+    }
+}
